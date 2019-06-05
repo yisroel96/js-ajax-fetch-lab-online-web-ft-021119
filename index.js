bf2749d6d6b4088fd681f488d21eb38879945457
maxwellbenton <maxwellbenton@gmail.com>
2019-06-05 16:52:42 -0400

    updates starter code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const baseURL = 'https://api.github.com';
+const user = '<YOUR_USERNAME>';
+
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
